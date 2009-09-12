c93e27c2f6ced91422175335fca73222eb8683ea
Owen Jacobson <owen.jacobson@grimoire.ca>
2009-09-12 00:23:37 -0400

    Docstrings and more comments.

--- redfox/__init__.py
+++ redfox/__init__.py
@@ -1,4 +1,22 @@
+"""Redfox provides a simple, declarative routing mechanism for creating
+WSGI entry points into applications. It's broadly similar to
+microframeworks like juno_ or CherryPy_.
+"""
+
 import redfox.meta
 
 class WebApplication(object):
+    """Web application classes should extend this class, rather than
+    using the ``redfox.meta.WebApplication`` metaclass. The following
+    example is a Hello World application::
+    
+        from redfox.routing import get
+        from werkzeug import Response
+        
+        class Example(WebApplication):
+            @get('/')
+            def index(self, request):
+                return Response('Hello, world!')
+    
+    """
     __metaclass__ = redfox.meta.WebApplication
--- redfox/meta.py
+++ redfox/meta.py
@@ -1,3 +1,7 @@
+"""The bowels of redfox. This is where the metaclass that makes Redfox
+app class instances into WSGI applications lives.
+"""
+
 from itertools import chain
 from werkzeug.routing import Map, Rule, RuleFactory
 from werkzeug import Request, ClosingIterator
@@ -5,8 +9,17 @@ from werkzeug.exceptions import HTTPException
 from redfox.routing import routes
 
 # Define a real, honest-to-Zod function named __call__ so that
-# webapp-derived types look as normal as possible.
+# webapp-derived types look as normal as possible. This function
+# gets reused for every single WebApplication-derived class's
+# __call__ method.
 def __call__(self, environ, start_response):
+    """WSGI entry point. Routes requests to methods decorated by ``@route``
+    and friends, then chains to the WSGI application (normally a
+    ``werkzeug.Response``) returned by the endpoint method.
+    
+    The WSGI request environment is encapsulated in a ``werkzeug.Request``
+    object, for convenience.
+    """
     request = Request(environ)
     adapter = self.__rule_map__.bind_to_environ(environ)
     try:
@@ -18,6 +31,20 @@ def __call__(self, environ, start_response):
     return ClosingIterator(response(environ, start_response))
 
 class WebApplication(type):
+    """Instances of this metaclass are given the following attributes:
+    
+    ``__rule_defs__``
+        A ``list`` of rule definitions suitable for constructing Rule objects.
+    
+    ``__rule_map__``
+        A ``werkzeug.routing.Map`` object for identifying the appropriate
+        method for each request. The ``Map`` is populated using rules from
+        both the class's own routable methods and from any parents that look
+        like they might have rule definitions.
+    
+    ``__call__``
+        A WSGI entry point method that uses ``__rule_map__`` to route requests.
+    """
     def __new__(meta, name, bases, dict):
         dict['__rule_defs__'] = rule_defs = list(meta.extract_rule_defs(dict))
         rules = [Rule(*args, **kwargs) for args, kwargs in rule_defs]
@@ -33,10 +60,20 @@ class WebApplication(type):
 
     @classmethod
     def extract_rule_defs(meta, attributes):
+        """Extracts rule definitions from all routable objects in the
+        ``attributes`` dictionary and returns them in a flat sequence.
+        """
         for name, method in attributes.iteritems():
             for rule in meta.to_rules(name, method):
                 yield rule
     
     @classmethod
     def to_rules(meta, name, method):
+        """Extracts the route definitions from a method, adding some
+        metadata necessary to route requests to methods.
+        """
+        # We use the name here so that requests routed to a child class
+        # prefer the child class's version of an overridden method, rather
+        # than the parent's. See __call__ for the other half of the
+        # equation.
         return routes(method, endpoint=name)
