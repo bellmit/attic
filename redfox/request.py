91dc8fc93d10643bf6d391c71e3dcbcd65483b75
Owen Jacobson <owen.jacobson@grimoire.ca>
2009-09-15 21:15:37 -0400

    Some basic docstrings.

--- redfox/request.py
+++ redfox/request.py
@@ -1,10 +1,13 @@
 from werkzeug import Request as WerkzeugRequest, BaseRequest
 
 class MapAdapterMixin(object):
+    """A request mixin that exposes a Werkzeug MapAdapter and its operations."""
+    
     def __init__(self, map_adapter):
         self.map_adapter = map_adapter
 
     def build(self, endpoint, values=None, method=None, force_external=False):
+        """Delegates to ``MapAdapter.build``."""
         return self.map_adapter.build(
             endpoint=endpoint,
             values=values,
@@ -19,6 +22,7 @@ class MapAdapterMixin(object):
         method=None,
         catch_http_exceptions=False
     ):
+        """Delegates to ``MapAdapter.dispatch``."""
         return self.map_adapter.dispatch(
             view_func=view_func,
             path_info=path_info,
@@ -27,13 +31,16 @@ class MapAdapterMixin(object):
         )
     
     def test(self, path_info=None, method=None):
+        """Delegates to ``MapAdapter.test``."""
         return self.map_adapter.test(path_info=path_info, method=method)
     
     def match(self, path_info=None, method=None):
+        """Delegates to ``MapAdapter.match``."""
         return self.map_adapter.match(path_info=path_info, method=method)
     
     def match_url(self, url, method=None):
-        # See <http://dev.pocoo.org/projects/werkzeug/ticket/414>
+        """Delegates to ``MapAdapter.match_url``, if available, or implements
+        it locally otherwise. See Werkzeug ticket #414 for details."""
         parsed_url = urlsplit(url)
         if self.subdomain:
             netloc = '%s.%s' % (self.subdomain, self.server_name)
@@ -51,6 +58,9 @@ class MapAdapterMixin(object):
         return self.match(path_info, method=method)
 
 class Request(WerkzeugRequest, MapAdapterMixin):
+    """A request type containing all of the types covered by
+    ``werkzeug.Request`` as well as ``MapAdapterMixin`` from this
+    module."""
     def __init__(self, map_adapter, *args, **kwargs):
         MapAdapterMixin.__init__(self, map_adapter)
         WerkzeugRequest.__init__(self, *args, **kwargs)
