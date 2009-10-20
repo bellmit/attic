985d9e3649b2f3bed9bc1b1bd9adf6ba6241b72a
Owen Jacobson <owen.jacobson@grimoire.ca>
2009-10-20 11:45:11 -0400

    Update version (1.2)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='redfox',
-    version='1.2.dev',
+    version='1.2',
     author='Owen Jacobson',
     author_email='owen.jacobson@grimoire.ca',
     
