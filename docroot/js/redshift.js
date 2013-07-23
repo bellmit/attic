38206db5d755d18f17bd1127ff8332d3e5c5f1cd
Owen Jacobson <owen@freshbooks.com>
2013-07-23 08:58:40 -0400

    Multiple routes.

--- docroot/js/redshift.js
+++ docroot/js/redshift.js
@@ -3,9 +3,11 @@ function( angular) {
 	return angular.module(
 		'redshift',
 		['redshift.lobby'],
-		function($routeProvider) {
+		function($locationProvider, $routeProvider) {
+			$locationProvider.html5Mode(true);
 			$routeProvider
 				.when('/', { templateUrl: '/fragments/lobby.html', controller: 'lobby' })
+				.when('/:gameid', { templateUrl: '/fragments/game.html', controller: 'lobby' })
 				.otherwise({redirectTo: '/'});
 		}
 	);
