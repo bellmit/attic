3a4bff226f381232b08dbb42703f3fcd9262936c
Owen Jacobson <owen.jacobson@grimoire.ca>
2014-09-21 15:38:24 -0400

    Customize the greeting when `/greet` receives a `name` query param.
    This passes the second test.

--- server/greeting.js
+++ server/greeting.js
@@ -5,7 +5,13 @@ var express = require('express');
 var route = express.Router();
 
 route.get('', function(req, res) {
-	res.send('Hello, world!');
+	var name = req.query.name || 'world';
+	var greeting = [
+		'Hello, ',
+		name,
+		'!'
+	].join('');
+	res.send(greeting);
 });
 
 module.exports = route;
