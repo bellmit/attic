c9348d7d3ae2a7cd53e9b65be1334e8deed78e47
Owen Jacobson <owen.jacobson@grimoire.ca>
2014-09-21 15:58:40 -0400

    Shiny: pulled the start/stop logic into its own helper.
    Now, instead of having to copy/paste the startup code into each test suite, it can be applied by name as `runServer()`. `jasmine-node` automatically imports every export from every `*.helpers.js` file into each test suite, so there's no need to explicitly `require()` the helpers.

--- test/server/greeting.spec.js
+++ test/server/greeting.spec.js
@@ -1,24 +1,9 @@
 'use strict';
 
-var child = require('child_process');
 var request = require('request');
 
 describe("The /greet endpoint", function () {
-	var server;
-
-	beforeEach(function(done) {
-		server = child.fork("server.js");
-		server.on('message', function(message) {
-			if (message == 'started') {
-				done();
-			}
-		});
-	});
-
-	afterEach(function(done) {
-		server.on('exit', done);
-		server.kill('SIGTERM');
-	});
+	runServer();
 
 	it("provides a generic greeting by default", function(done) {
 		request('http://localhost:5000/greet', function(error, response, body) {
