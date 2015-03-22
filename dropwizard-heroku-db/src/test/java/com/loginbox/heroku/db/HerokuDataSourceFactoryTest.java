039e564955088667af67b48219343e60d522ea6b
Owen Jacobson <owen.jacobson@grimoire.ca>
2015-03-21 20:17:34 -0400

    Allow URLs with no credentials.
    These are useful when testing: for example, Postgres.app permits (local) TCP/IP
    logins with no username.

--- dropwizard-heroku-db/src/test/java/com/loginbox/heroku/db/HerokuDataSourceFactoryTest.java
+++ dropwizard-heroku-db/src/test/java/com/loginbox/heroku/db/HerokuDataSourceFactoryTest.java
@@ -23,6 +23,18 @@ public class HerokuDataSourceFactoryTest {
     }
 
     @Test
+    public void permitsMissingCredentials() throws IOException, InterruptedException {
+        HerokuDataSourceFactory f = run(
+                HerokuDataSourceFactory.class,
+                set("DATABASE_URL", "postgres://db.example.com/database")
+        );
+        assertThat(f.getDriverClass(), is("org.postgresql.Driver"));
+        assertThat(f.getUrl(), is("jdbc:postgresql://db.example.com/database"));
+        assertThat(f.getUser(), is(nullValue()));
+        assertThat(f.getPassword(), is(""));
+    }
+
+    @Test
     public void noEnvironmentNoConfig() throws IOException, InterruptedException {
         HerokuDataSourceFactory f = run(
                 HerokuDataSourceFactory.class,
