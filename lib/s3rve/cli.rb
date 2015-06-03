3c89bc1e58503e8fca6d0499704c7d12c510b388
Owen Jacobson <owen@heroku.com>
2015-06-03 12:27:19 -0400

    Nicer CLI, one site at a time.

--- lib/s3rve/cli.rb
+++ lib/s3rve/cli.rb
@@ -2,11 +2,17 @@ require 'slop'
 
 module S3rve
   module Cli
+    def self.usage(opts)
+      puts opts
+      exit 1
+    end
+    
     def self.options
       Slop.parse do |o|
+        o.banner = "usage: s3rve [options] path/to/static.json"
+
         o.on '-?', '--help', "show this help message, then exit" do
-          puts o
-          exit
+          usage o
         end
       end
     end
