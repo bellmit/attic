d4861bd22fc430c07c7f351e8ae4a826885927da
Owen Jacobson <owen.jacobson@grimoire.ca>
2015-08-26 19:05:16 -0400

    Use Nickel.
    I'm using a Git repo here, because the last release won't compile with Rust
    nightly:
    
        src/server.rs:22:26: 22:57 error: cannot infer an appropriate lifetime for lifetime parameter `'a` due to conflicting requirements
        src/server.rs:22         let nickel_req = request::Request::from_internal(req);
                                                  ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        src/server.rs:21:5: 26:6 help: consider using an explicit lifetime parameter as shown: fn handle<'a>(&'a self, req: Request<'a, 'a>, res: Response<'a>)
        src/server.rs:21     fn handle<'a, 'k>(&'a self, req: Request<'a, 'k>, res: Response<'a>) {
        src/server.rs:22         let nickel_req = request::Request::from_internal(req);
        src/server.rs:23         let nickel_res = response::Response::from_internal(res, &self.0.templates);
        src/server.rs:24
        src/server.rs:25         self.0.middleware_stack.invoke(nickel_req, nickel_res);
        src/server.rs:26     }
        error: aborting due to previous error
        Could not compile `nickel`.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[macro_use] extern crate nickel;
+
+use nickel::Nickel;
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            "Hello world!"
+        }
+    });
+
+    server.listen("0.0.0.0:5000");
 }
