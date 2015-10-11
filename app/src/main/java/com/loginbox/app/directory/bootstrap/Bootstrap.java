bfd43c590b8ff1e1c5d3e0103c0cc6b3f5ba9cf5
Owen Jacobson <owen.jacobson@grimoire.ca>
2015-10-11 01:41:50 -0400

    Just for giggles, a simpler bootstrap action.
    The composition operators are _cool_, but they're not _readable_, and as
    mentioned they're a hazard to refactoring.

--- app/src/main/java/com/loginbox/app/directory/bootstrap/Bootstrap.java
+++ app/src/main/java/com/loginbox/app/directory/bootstrap/Bootstrap.java
@@ -6,13 +6,8 @@ import com.loginbox.app.directory.internal.InternalDirectoryQueries;
 import com.loginbox.app.identity.User;
 import com.loginbox.app.identity.UserInfo;
 import com.loginbox.app.password.PasswordValidator;
-import com.loginbox.transactor.transactable.Action;
-import com.loginbox.transactor.transactable.Merge;
-import com.loginbox.transactor.transactable.Query;
 import com.loginbox.transactor.transactable.Transform;
 import org.apache.ibatis.session.SqlSession;
-
-import static com.loginbox.app.transactor.mybatis.MybatisAdapters.mapper;
 
 public abstract class Bootstrap {
     private final Gatekeeper setupGatekeeper;
@@ -21,44 +16,23 @@ public abstract class Bootstrap {
         this.setupGatekeeper = setupGatekeeper;
     }
 
-    private static <C, I, O> Transform<I, C, O> pivot(Transform<? super C, ? super I, ? extends O> t) {
-        return (context, input) -> t.apply(input, context);
-    }
-
     public Transform<SqlSession, UserInfo, User> setupAction() {
-        Query<SqlSession, PasswordValidator> passwordValidator
-                = Query.lift(this::getPasswordValidator);
+        PasswordValidator passwordValidator = getPasswordValidator();
 
-        Merge<SqlSession, UserInfo, PasswordValidator, UserInfo> digestUserInfoAgainstValidator
-                = Merge.lift(UserInfo::withDigestedPassword);
+        Transform<SqlSession, UserInfo, User> setupAction = (session, userInfo) -> {
+            UserInfo digestedUserInfo = userInfo.withDigestedPassword(passwordValidator);
 
-        Transform<SqlSession, Directories, InternalDirectory> createInternalDirectoryInDirectories
-                = pivot(Directories::createInternalDirectory);
+            Directories directories = getDirectories();
+            InternalDirectory internalDirectory = directories.createInternalDirectory(session);
 
-        Merge<SqlSession, InternalDirectory, UserInfo, User> addInitialUserToDirectory
-                = mapper(InternalDirectoryQueries.class)
-                .around(InternalDirectoryQueries::insertUser);
+            User user = session
+                    .getMapper(InternalDirectoryQueries.class)
+                    .insertUser(internalDirectory, digestedUserInfo);
 
-        Action<SqlSession> bootstrapCompletedAction = setupGatekeeper.bootstrapCompletedAction();
-
-        Query<SqlSession, Directories> directories = Query.lift(this::getDirectories);
-
-        Query<SqlSession, InternalDirectory> createInternalDirectory
-                = directories.transformedBy(createInternalDirectoryInDirectories);
-
-        Transform<SqlSession, UserInfo, User> addInitialUserToNewDirectory
-                = createInternalDirectory.intoLeft(addInitialUserToDirectory);
-
-        Transform<SqlSession, UserInfo, UserInfo> digestUserInfo
-                = passwordValidator.intoRight(digestUserInfoAgainstValidator);
-
-        Transform<SqlSession, UserInfo, User> addDigestedInitialUserToNewDirectory
-                = digestUserInfo.transformedBy(addInitialUserToNewDirectory);
-
-        Transform<SqlSession, UserInfo, User> bootstrap = addDigestedInitialUserToNewDirectory
-                .andThen(bootstrapCompletedAction);
-
-        return bootstrap;
+            return user;
+        };
+        return setupAction
+                .andThen(setupGatekeeper.bootstrapCompletedAction());
     }
 
     protected abstract PasswordValidator getPasswordValidator();
