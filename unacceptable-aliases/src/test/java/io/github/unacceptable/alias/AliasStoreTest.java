b41ddc3efd16ac9148fe4918ad905d959a517231
Owen Jacobson <owen.jacobson@grimoire.ca>
2015-11-08 14:51:04 -0500

    Added Derek's AliasStore improvements:
    * `null` handling
    * `"ABSENT"` magic value for DSL tests
    * `AliasStore.store(alias, name)` value-forcing method

--- unacceptable-aliases/src/test/java/io/github/unacceptable/alias/AliasStoreTest.java
+++ unacceptable-aliases/src/test/java/io/github/unacceptable/alias/AliasStoreTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
 
 public class AliasStoreTest {
     @Test
@@ -14,20 +15,51 @@ public class AliasStoreTest {
     }
 
     @Test
+    public void resolvesNull() {
+        AliasStore aliases = new AliasStore(this::failIfCalled);
+
+        assertThat(aliases.resolve(null), nullValue());
+    }
+
+    @Test
+    public void resolvesAbsent() {
+        AliasStore aliases = new AliasStore(this::failIfCalled);
+
+        assertThat(aliases.resolve("ABSENT"), nullValue());
+    }
+
+    @Test
+    public void resolvesStoredValue() {
+        AliasStore aliases = new AliasStore(this::failIfCalled);
+
+        aliases.store("alias", "value");
+        assertThat(aliases.resolve("alias"), equalTo("value"));
+    }
+
+    @Test
+    public void resolvesStoredNull() {
+        AliasStore aliases = new AliasStore(this::failIfCalled);
+
+        aliases.store("alias", null);
+        assertThat(aliases.resolve("alias"), nullValue());
+    }
+
+    @Test
     public void generatesFixedValues() {
-        AliasStore aliases = new AliasStore(alias -> {
-            throw new RuntimeException("nope");
-        });
+        AliasStore aliases = new AliasStore(this::failIfCalled);
 
         assertThat(aliases.resolve("<value>"), equalTo("value"));
     }
 
     @Test
     public void generatesEmptyValue() {
-        AliasStore aliases = new AliasStore(alias -> {
-            throw new RuntimeException("nope");
-        });
+        AliasStore aliases = new AliasStore(this::failIfCalled);
 
         assertThat(aliases.resolve("<>"), equalTo(""));
     }
+
+    private String failIfCalled(String alias) {
+        throw new RuntimeException("generator should not be called");
+    }
 }
+
