cf863779c047157bbbc4d6ced07f260d06802db0
Owen Jacobson <owen.jacobson@grimoire.ca>
2016-05-10 23:06:34 -0400

    Add ReactJS UI.
    This uses Webpack directly to do the build, rather than a wrapper like Gulp, so
    that it's easier to use Webpack-specific features (such as `--watch`,
    demonstrated here).

---
+++ js/App/routes/routes/Squad/index.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    path: 'squad',
+
+    getComponents(nextState, callback) {
+        require.ensure([], (require) => {
+            callback(null, require('./components/Squad'));
+        });
+    },
+};
