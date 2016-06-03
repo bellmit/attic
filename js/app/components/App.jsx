d2a1c2cfc1a45b783a34081c425a2cdd8d163c7e
Owen Jacobson <owen.jacobson@grimoire.ca>
2016-06-02 22:53:38 -0400

    Log in using Auth0.
    I've included scratch credentials (which work, but are restricted to the "Dev"
    app) to index.html. I don't feel _fantastic_ about this, but I don't have a
    better idea for providing dev credentials. So far as I can tell, you can only
    use these to initiate authentication; you can't use them to _validate_ tokens
    or to construct new ones.
    
    Authentication state is mediated through Redux, like all other app state. This
    is loosely based on <https://auth0.com/blog/2016/01/04/secure-your-react-and-redux-app-with-jwt-authentication/>.
    The profile screen is temporary; I needed a way to sanity-check login state.

--- js/app/components/App.jsx
+++ js/app/components/App.jsx
@@ -3,10 +3,25 @@
 import React from 'react';
 import DocumentTitle from 'react-document-title';
 
-function App({children}) {
-  return <DocumentTitle title="Distant Shore">
-    {children}
-  </DocumentTitle>;
-}
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { boot } from 'app/lock/actions';
 
-module.exports = App;
+const App = React.createClass({
+  componentDidMount() {
+    this.props.boot();
+  },
+
+  render() {
+    var {children} = this.props;
+
+    return <DocumentTitle title="Distant Shore">
+      {children}
+    </DocumentTitle>;
+  },
+});
+
+module.exports = connect(
+  state => ({}),
+  dispatch => bindActionCreators({boot}, dispatch)
+)(App);
--- js/app/components/Landing.jsx
+++ js/app/components/Landing.jsx
@@ -2,11 +2,35 @@
 
 import React from 'react';
 import { Link } from 'react-router';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import jwtDecode from 'jwt-decode';
+import * as actions from 'app/lock/actions';
 
-function Landing() {
-  return <ul>
-    <li><Link to="squad">Squad</Link></li>
-  </ul>;
+function Landing({idToken, profile, login, logout}) {
+  return <div className="container">
+    <div className="col-md-3">
+      <ul>
+        <li><Link to="squad">Squad</Link></li>
+      </ul>
+    </div>
+    <div className="col-md-9">
+      <p>ID token: {idToken}</p>
+      {idToken &&
+        <pre>{JSON.stringify(jwtDecode(idToken), null, "  ")}</pre>
+      }
+      <p>Profile:</p>
+      {profile &&
+        <pre>{JSON.stringify(profile, null, "  ")}</pre>}
+      {idToken ?
+        <button className="btn btn-default" onClick={logout}>Log out</button> :
+        <button className="btn btn-default" onClick={login}>Log in</button>
+      }
+    </div>
+  </div>;
 }
 
-module.exports = Landing;
+module.exports = connect(
+  state => state.lock,
+  dispatch => bindActionCreators(actions, dispatch)
+)(Landing);
