d903cab0e930c528228373ed29684619c2edf3bd
Owen Jacobson <owen.jacobson@grimoire.ca>
2016-06-18 17:20:47 -0400

    Introduced a subscriber combinator to simplify subscribing to nested states.
    This combinator also handles deduping events, so that callees are only invoked
    when the requisite portion of the state actually changes.

---
+++ js/app/subscribe.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import keys from 'lodash.keys';
+
+function keySubscriber(key, callback) {
+  var last = undefined;
+  return state => {
+    var next = state === undefined ? undefined : state[key];
+    if (last === undefined || last !== next) {
+      last = next;
+      callback(next);
+    }
+  };
+}
+
+function reduceSubscribers(first, next) {
+  return (...args) => {
+    first(...args);
+    next(...args);
+  };
+}
+
+export default function subscribe(subscribers) {
+  return keys(subscribers)
+    .map(key => keySubscriber(key, subscribers[key]))
+    .reduce(reduceSubscribers);
+}
+
+export function mount(store, subscriber) {
+  function listener() {
+    var state = store.getState();
+    subscriber(state);
+  }
+  return store.subscribe(listener);
+}
