410794266bf49d0bfbabfb76407f785c7ea2c5f8
Owen Jacobson <owen.jacobson@grimoire.ca>
2016-06-18 16:22:09 -0400

    Streamlined landing flow for non-authenticated users.
    The call to action is much clearer: log the fuck in, or sign up.

--- js/app/components/Landing.jsx
+++ js/app/components/Landing.jsx
@@ -7,7 +7,7 @@ import { connect } from 'react-redux';
 import jwtDecode from 'jwt-decode';
 import * as actions from 'app/lock/actions';
 
-function Landing({idToken, profile, userId, login, logout}) {
+function LoggedInLanding({idToken, profile, userId, login, logout}) {
   return <div className="container">
     <div className="col-md-3">
       <ul>
@@ -16,9 +16,7 @@ function Landing({idToken, profile, userId, login, logout}) {
     </div>
     <div className="col-md-9">
       <p>ID token:</p>
-      {idToken &&
-        <pre>{JSON.stringify(jwtDecode(idToken), null, "  ")}</pre>
-      }
+      <pre>{JSON.stringify(jwtDecode(idToken), null, "  ")}</pre>
       <p>API Identity: {
         userId ?
           <span className="glyphicon glyphicon-ok"></span> :
@@ -27,12 +25,47 @@ function Landing({idToken, profile, userId, login, logout}) {
       <p>Profile:</p>
       {profile &&
         <pre>{JSON.stringify(profile, null, "  ")}</pre>}
-      {idToken ?
-        <button className="btn btn-default" onClick={logout}>Log out</button> :
-        <button className="btn btn-default" onClick={login}>Log in</button>
-      }
+      <button className="btn btn-default" onClick={logout}>Log out</button>
     </div>
   </div>;
+}
+
+function LandingCopy() {
+  return <div className="jumbotron">
+    <h1>Distant Shore</h1>
+    <p>
+      Knausgaard taxidermy gochujang polaroid. Lo-fi truffaut schlitz,
+      gluten-free forage pickled biodiesel beard meggings keytar art party
+      stumptown slow-carb craft beer.
+    </p>
+    <p>
+      In the finished app, this should be a preview of a live fight, or a
+      recording.
+    </p>
+  </div>;
+}
+
+const AnonymousLanding = React.createClass({
+  componentDidMount() {
+    this.props.login({
+      container: "auth0-lock",
+    })
+  },
+
+  render() {
+    return <div className="container">
+      <div id="auth0-lock" className="col-md-4"/>
+      <div className="col-md-8">
+        <LandingCopy />
+      </div>
+    </div>
+  },
+});
+
+function Landing({idToken, ...props}) {
+  if (!idToken)
+    return <AnonymousLanding {...props} />
+  return <LoggedInLanding idToken={idToken} {...props} />
 }
 
 module.exports = connect(
