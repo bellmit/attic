9deb22cf38fbb1629e40caa89caacd428bcdae9c
Owen Jacobson <owen.jacobson@grimoire.ca>
2016-07-21 19:38:03 -0400

    Unify common settings.

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -4,27 +4,16 @@ module.exports = {
     "commonjs": true,
     "es6": true,
   },
-  "extends": "eslint:recommended",
   "parserOptions": {
     "ecmaFeatures": {
       "experimentalObjectRestSpread": true,
       "jsx": true,
     },
-    "sourceType": "module",
   },
-  "plugins": [
-    "react",
-  ],
   "rules": {
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "semi": ["error", "never"],
     "no-console": ["error", {
       allow: ["warn", "error"],
     }],
-    "react/jsx-uses-vars": ["error"],
-    "react/jsx-uses-react": ["error"],
-    "react/react-in-jsx-scope": ["error"],
   },
   "globals": {
     "appConfig": false,
--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -5,25 +5,14 @@ module.exports = {
     "es6": true,
     "mocha": true,
   },
-  "extends": "eslint:recommended",
   "parserOptions": {
     "ecmaFeatures": {
       "experimentalObjectRestSpread": true,
       "jsx": true,
     },
-    "sourceType": "module",
   },
-  "plugins": [
-    "react",
-  ],
   "rules": {
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "semi": ["error", "never"],
     "no-console": ["error"],
-    "react/jsx-uses-vars": ["error"],
-    "react/jsx-uses-react": ["error"],
-    "react/react-in-jsx-scope": ["error"],
   },
   "globals": {
     "expect": false,
