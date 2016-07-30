ac4c6c9494faa59679b7b0373d5d8fe2f2873d6b
Owen Jacobson <owen.jacobson@grimoire.ca>
2016-07-29 22:29:26 -0400

    Clean up the landing UI. It now offers options, and doesn't have UL Syndrome.

--- src/app/components/Landing/LoggedIn.js
+++ src/app/components/Landing/LoggedIn.js
@@ -1,16 +1,43 @@
 import React from 'react'
 import { Link } from 'react-router'
-import Lobby from 'app/components/Lobby'
+import Copy from './Copy'
 
 export default function LoggedIn({logout, lock}) {
   return <div className="container">
-    <h1>Play Now</h1>
-    <div className="col-md-3">
-      <ul>
-        <li><Link to="squad">Squad</Link></li>
-        <li><a role="button" onClick={() => logout(lock)}>Log out</a></li>
-      </ul>
+    <div className="list-group col-md-4">
+      <Link to="squad" className="list-group-item">
+        <h2 className="list-group-item-heading">Edit your squad</h2>
+        <p className="list-group-item-text">
+          Rebuild your squad and come back stronger than ever.
+        </p>
+      </Link>
+      <Link className="list-group-item" to="/battle/123">
+        <h2 className="list-group-item-heading">Challenge a friend</h2>
+        <p className="list-group-item-text">
+          DIY normcore wayfarers godard, truffaut cold-pressed occupy forage.
+        </p>
+      </Link>
+      <Link className="list-group-item" to="/battle/123">
+        <h2 className="list-group-item-heading">Ranked battle</h2>
+        <p className="list-group-item-text">
+          Take on challengers to prove you're the best there is.
+        </p>
+      </Link>
+      <Link className="list-group-item" to="/battle/123">
+        <h2 className="list-group-item-heading">Practice battle</h2>
+        <p className="list-group-item-text">
+          Protect the land from rampaging monsters and practice with your squad.
+        </p>
+      </Link>
+      <button className="list-group-item" onClick={() => logout(lock)}>
+        <h2 className="list-group-item-heading">Log out</h2>
+        <p className="list-group-item-text">
+          Protect your account when playing at internet cafés or on shared computers.
+        </p>
+      </button>
     </div>
-    <Lobby className="col-md-9" />
+    <div className="col-md-8">
+      <Copy />
+    </div>
   </div>
 }
--- src/app/components/Lobby.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { Link } from 'react-router'
-
-export default function Lobby(props) {
-  return <div {...props}>
-    <Link to="/battle/123" className="btn btn-default">Challenge a friend</Link>
-  </div>
-}
