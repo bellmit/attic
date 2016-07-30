510107593227d729c4081e882f9ece8cd909dae2
Owen Jacobson <owen.jacobson@grimoire.ca>
2016-07-29 23:33:13 -0400

    A tooter button

--- src/app/lobby/components/Challenge.js
+++ src/app/lobby/components/Challenge.js
@@ -5,6 +5,9 @@ module.exports = function Challenge() {
   return <div className="container">
     <h1>Challenge your friend</h1>
     <p>Social media buttons for posting the challenge here</p>
+    <a href="https://twitter.com/share"
+      className="twitter-share-button"
+      data-text="I'm playing Distant Shore. Challenge me!">Tweet</a>
     <p>Or copy this link and share it yourself.</p>
     <hr />
     <ul>
