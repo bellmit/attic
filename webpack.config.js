a7c5d45580212203694fe437e137fecaf118a090
Owen Jacobson <owen.jacobson@grimoire.ca>
2016-09-30 22:51:21 -0400

    Drop dead code using UglifyJS.
    This is a bit heavy-handed, but it seems to be the only way to actually remove
    unused code from the bundle. Since the whole point of dead code removal is to
    shrink the bundle…

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -15,6 +16,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
     }),
   ],
 
